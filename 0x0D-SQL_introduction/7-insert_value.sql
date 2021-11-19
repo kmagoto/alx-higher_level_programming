34611c0513ddc2472eaaca0ceb9fbff2a73cfa53
kmagoto <kennedymagoto@gmail.com>
2021-11-19 05:22:50 -0800

    initial commit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts new row in first_table in db hbtn_0c_0
 
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, `Holberton School`);
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
