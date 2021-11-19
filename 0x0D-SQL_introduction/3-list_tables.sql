d491a36184267c1bf9408c7a512d564be23d70be
kmagoto <kennedymagoto@gmail.com>
2021-11-19 05:24:39 -0800

    initial commit

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- lists all tables of database in server
+-- cat [filename] | mysql -hlocalhost -uroot -p mysql
+
+SHOW tables;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- prints full description of table first_table from db hbtn_0c_0 in server
+--
+-- Table   Create Table
+-- first_table CREATE TABLE `first_table` (\n  `id` int(11) DEFAULT NULL,
+-- \n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+SHOW CREATE TABLE first_table;
